e4707b1661c60b7282bbea810003abbe95e35f57
raki <rakeshhotker@gmail.com>
2020-12-31 22:10:38 +0530

    New year wishing

---
+++ happy.js
@@ -0,0 +1,37 @@
+var btn1=document.getElementById('btn1');
+var rahul=document.getElementById('wish-rahul');
+var btn2=document.getElementById('btn2');
+var Ashutosh=document.getElementById('wish-ashutosh');
+var btn3=document.getElementById('btn3');
+var siddharth=document.getElementById('wish-siddharth');
+var btn4=document.getElementById('btn4');
+var vamshi=document.getElementById('wish-vamshi');
+var btn5=document.getElementById('btn5');
+var sumanth=document.getElementById('wish-sumanth');
+var btn6=document.getElementById('btn6');
+var maheshChandra=document.getElementById('wish-mahesh');
+btn1.addEventListener('click',function()
+{
+    btn1.style.display='none';
+    rahul.innerHTML='Happy New Year Everybody';
+})
+// btn2.addEventListener('click',function()
+// {
+//     Ashutosh.innerHTML='Happy New Year Aau!';
+// })
+// btn3.addEventListener('click',function()
+// {
+//     siddharth.innerHTML='Happy New Year Mama';
+// })
+// btn4.addEventListener('click',function()
+// {
+//     vamshi.innerHTML='Happy New Year Mami';
+// })
+// btn5.addEventListener('click',function()
+// {
+//     sumanth.innerHTML='Happy New Year Ashwini';
+// })
+// btn6.addEventListener('click',function()
+// {
+//     mahesh.innerHTML='Happy New Year Bharath';
+// })
